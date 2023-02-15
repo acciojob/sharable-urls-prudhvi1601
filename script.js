22a66ae6cb05c6428f41084963b62a304cea5c1a
Prudhvi Raj <120363980+prudhvi1601@users.noreply.github.com>
2023-02-16 03:24:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-// your code here
+var form = document.querySelector("form");
+var h3 = document.querySelector("h3");
+var nameElement = document.querySelector("#name");
+var yearElement = document.querySelector("#year");
+var btn = document.querySelector("#button");
+ 
+function sharableURL(event) {
+	event.preventDefault();
+	var h3value = "https://localhost:8080/"
+	var name = nameElement.value;
+	var year = yearElement.value;
+	
+	if(name && year){
+		h3value += "?name=" + name + "&year=" + year;
+	}
+	else if(name && !year){
+		h3value += "?name=" + name;
+	}
+	else if(!name && year){
+		h3value += "?year=" + year;
+	}
+	h3.textContent = h3value;
+}
+form.addEventListener("submit",sharableURL);
